dd2d8176f818691a92833ef335d6f505693cf605
Kim Mattiko <amberlie@rocketmail.com>
2015-05-12 22:25:07 -0400

    adding new JS code and work in HTML

--- NestedAccordion/scripts.js
+++ NestedAccordion/scripts.js
@@ -1,97 +1,31 @@
-/**
- * jquery.cbpNTAccordion.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2013, Codrops
- * http://www.codrops.com
- */
-;( function( $, window, undefined ) {
+//just a tracer...
+//alert("It's alive!");
 
-	'use strict';
+document.head.parentElement.className = "js";
 
-	// global
-	var $body = $( 'html, body' );
+var elements = document.querySelectorAll('h3');
 
-	$.CBPNTAccordion = function( options, element ) {
-		this.$el = $( element );
-		this._init( options );
-	};
+//elements[0].addEventListener("click", function(){
+//	elements[0].parentElement.classList.toggle('cbp-ntopen');
+//	});
+//li.classList.toggle('cbp-ntopen');
 
-	// the options
-	$.CBPNTAccordion.defaults = {};
 
-	$.CBPNTAccordion.prototype = {
-		_init : function( options ) {
-			// options
-			this.options = $.extend( true, {}, $.CBPNTAccordion.defaults, options );
-			// cache some elements and initialize some variables
-			this._config();
-			// initialize/bind the events
-			this._initEvents();
-		},
-		_config : function() {
 
-			// the clickable items
-			this.$items = this.$el.find( '.cbp-nttrigger' );
 
-		},
-		_initEvents : function() {
-			
-			this.$items.on( 'click.cbpNTAccordion', function() {
-				var $listItem = $( this ).parent();
-				if( $listItem.hasClass( 'cbp-ntopen' ) ) {
-					$listItem.removeClass( 'cbp-ntopen' );
-				}
-				else {
-					$listItem.addClass( 'cbp-ntopen' );
-					$body.scrollTop( $listItem.offset().top );
-				}
-			} );
 
-		},
-		destroy : function() {
-			this.$items.off( '.cbpNTAccordion' ).parent().removeClass( 'cbp-ntopen' );
-		}
-	};
+_.forEach(elements, function(element, index, elements){
+    element.addEventListener('click', function(){
+        console.log(elements, index, element);
+        element.parentElement.classList.toggle('cbp-ntopen');
+    });
+});
 
-	var logError = function( message ) {
-		if ( window.console ) {
-			window.console.error( message );
-		}
-	};
+var details = document.querySelectorAll("h4");
 
-	$.fn.cbpNTAccordion = function( options ) {
-		if ( typeof options === 'string' ) {
-			var args = Array.prototype.slice.call( arguments, 1 );
-			this.each(function() {
-				var instance = $.data( this, 'cbpNTAccordion' );
-				if ( !instance ) {
-					logError( "cannot call methods on cbpNTAccordion prior to initialization; " +
-					"attempted to call method '" + options + "'" );
-					return;
-				}
-				if ( !$.isFunction( instance[options] ) || options.charAt(0) === "_" ) {
-					logError( "no such method '" + options + "' for cbpNTAccordion instance" );
-					return;
-				}
-				instance[ options ].apply( instance, args );
-			});
-		} 
-		else {
-			this.each(function() {	
-				var instance = $.data( this, 'cbpNTAccordion' );
-				if ( instance ) {
-					instance._init();
-				}
-				else {
-					instance = $.data( this, 'cbpNTAccordion', new $.CBPNTAccordion( options, this ) );
-				}
-			});
-		}
-		return this;
-	};
-
-} )( jQuery, window );
+_.forEach(details, function(detail, index, details){
+    detail.addEventListener('click', function(){
+        console.log(details, index, detail);
+        detail.parentElement.classList.toggle('cbp-ntopen');
+    });
+});
